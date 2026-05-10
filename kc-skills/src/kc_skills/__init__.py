92d675570a53e6d07cba09edd4b47c09d3df6b12
Sammy Dallal <sammydallal@gmail.com>
2026-05-10 14:10:39 -0700

    feat(kc-skills): build_skill_tools factory (skills_list, skill_view, skill_run_script)

--- kc-skills/src/kc_skills/__init__.py
+++ kc-skills/src/kc_skills/__init__.py
@@ -1,1 +1,27 @@
+"""kc-skills public exports."""
+
 __version__ = "0.1.0"
+
+from kc_skills.frontmatter import (
+    FrontmatterError,
+    parse_skill_frontmatter,
+    skill_matches_platform,
+)
+from kc_skills.skill_index import (
+    PathOutsideSkillDir,
+    Skill,
+    SkillIndex,
+    SkillSummary,
+)
+from kc_skills.tools import build_skill_tools
+
+__all__ = [
+    "FrontmatterError",
+    "parse_skill_frontmatter",
+    "skill_matches_platform",
+    "PathOutsideSkillDir",
+    "Skill",
+    "SkillIndex",
+    "SkillSummary",
+    "build_skill_tools",
+]
