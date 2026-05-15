f61324d3e967dbe36b43b645dde2caac749af17e
Sammy Dallal <sammydallal@gmail.com>
2026-05-15 09:10:09 -0700

    chore(kc-web): surface OllamaClient + WebClientError at package level

--- kc-web/src/kc_web/__init__.py
+++ kc-web/src/kc_web/__init__.py
@@ -1,6 +1,25 @@
 """KonaClaw web tools (web_search + web_fetch)."""
 
+from kc_web.client import (
+    FirecrawlClient,
+    FirecrawlError,
+    OllamaClient,
+    ScrapeResult,
+    SearchResult,
+    WebClient,
+    WebClientError,
+)
 from kc_web.config import WebConfig
 from kc_web.tools import build_web_tools
 
-__all__ = ["WebConfig", "build_web_tools"]
+__all__ = [
+    "FirecrawlClient",
+    "FirecrawlError",
+    "OllamaClient",
+    "ScrapeResult",
+    "SearchResult",
+    "WebClient",
+    "WebClientError",
+    "WebConfig",
+    "build_web_tools",
+]
